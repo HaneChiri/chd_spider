d2c5b61bb90224ffd5832d647396d40b360d041a
HaneChiri <featherdust@126.com>
2019-04-14 21:03:26 +0800

    create class parser

--- learn_xpath.py
+++ learn_xpath.py
@@ -1,12 +1,55 @@
 from lxml import etree
 import requests
-url='https://www.biquge.biz/2_2224/'
 
-response=requests.get(url)
-html=etree.HTML(response.text)
-content=html.xpath('/html/head/title')
+xml='''
 
-print(content.text)
 
-for i in content:
-    print(i.text)
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-878633-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-878633-1');
+</script>
+
+<meta charset="gbk" />
+<meta name="robots" content="all" />
+<meta name="author" content="w3school.com.cn" />
+<link rel="stylesheet" type="text/css" href="/c5.css" />
+<link rel="icon" type="image/png" sizes="16x16" href="/ui2019/w3_16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/ui2019/w3_32x32.png">
+<link rel="icon" type="image/png" sizes="48x48" href="/ui2019/logo-48-red.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/ui2019/logo-96-red.png">
+<link rel="apple-touch-icon-precomposed" sizes="180x180" href="/ui2017/logo-180.png">
+
+
+<title>XPath</title>
+
+</head>
+
+<body class="xml">
+
+<div id="wrapper">
+...
+
+
+</div>
+</body>
+</html>
+'''
+
+
+xml=etree.HTML(xml)
+list=xml.xpath('/html')
+
+print(type(list))
+
+print(list)
+for i in list:
+    print(i)
--- spider_class.py
+++ spider_class.py
@@ -2,12 +2,92 @@ import requests
 from bs4 import BeautifulSoup
 from lxml import etree
 
+
+class parser():
+    def chd_login_data_parser(self,url):
+        '''
+        This parser is for chd
+        :param url: the url you want to login
+        :return (a dict with login data,cookies)
+        '''
+        response=requests.get(login_url)
+        html=response.text
+        # parse the html
+        soup=BeautifulSoup(html,'lxml')
+        lt=soup.find('input',{'name':'lt'})['value']
+        dllt=soup.find('input',{'name':'dllt'})['value']
+        execution = soup.find('input', {'name': 'execution'})['value']
+        _eventId = soup.find('input', {'name': '_eventId'})['value']
+        rmShown = soup.find('input', {'name': 'rmShown'})['value']
+        login_data={
+            'username': input('input account:'),
+            'password': input('input passwd:'),
+            'btn':'',
+            'lt': lt,
+            'dllt': dllt,
+            'execution': execution,
+            '_eventId': _eventId,
+            'rmShown': rmShown
+        }
+        return login_data,response.cookies
+
+    def ucard_login_data_parser(self,url):
+        pass
+        '''
+        response=requests.get(url)
+        html=response.text
+        # parse the html
+        soup=BeautifulSoup(html,'lxml')
+        print(soup.prettify())
+        next_url = soup.find('input', {'name': 'NextUrl'})['value']
+        check_code=''
+        img_url= soup.find('img', {'id': 'imgCheckCode'})['src']
+        img_url='http://api.xzxyun.com/Account/'+img_url
+        img=self.get_img(img_url,'check_code')
+
+        print(img)
+        #'http://api.xzxyun.com/Account/Login/Account/GetCheckCodeImg?rad=9'
+        #http://api.xzxyun.com/Account/GetCheckCodeImg?rad=52
+
+        openid=''
+        login_data={
+            'SchoolCode':'xahu',#input('input your school name'),
+            'SignType':'SynSno',#input('input your SignType,"SynSno" or "SynCard"')
+            'UserAccount':input('input your User Account'),
+            'Password':input('input your Password'),
+            'NextUrl':next_url,
+            'CheckCode':check_code,
+            'openid':openid,
+        }
+        
+        return login_data,response.cookies
+        '''
+    
+
+    def uni_parser(self,url,xpath,**kwargs):
+        html=requests.post(url,**kwargs).text
+        tree=etree.HTML(html)
+        result_list=tree.xpath(xpath)
+        return result_list
+    
+    def chd_get_urls(self,url,**kwargs):
+        pass
+        #get page number
+        xpath='//div[@class="pagination-info clearFix"]/span[0]/text()'
+        x=self.uni_parser(url,xpath,**kwargs)
+        print(x)
+        #repeat get single catalogue's urls
+
+
+
+
 class spider(object):
     def __init__(self):
         self.cookies=None
         self.headers={
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
         }
+        self.parser=parser()#spider's parser
         
     
 
@@ -38,9 +118,8 @@ class spider(object):
         self.cookies=cookies
         return response
         
-    def get_urls(self,url):
-        pass
-            
+    
+        
 
     def get_img(self,url,output_name=None,cookies=None):
         '''
@@ -61,70 +140,13 @@ class spider(object):
 
         return img
 
-    
-    def parse(self):
-        pass
 
     def save(self):
         pass
 
 
 
-    def chd_login_data_parser(self,url):
-        '''
-        This parser is for chd
-        :param url: the url you want to login
-        :return (a dict with login data,cookies)
-        '''
-        response=requests.get(login_url)
-        html=response.text
-        # parse the html
-        soup=BeautifulSoup(html,'lxml')
-        lt=soup.find('input',{'name':'lt'})['value']
-        dllt=soup.find('input',{'name':'dllt'})['value']
-        execution = soup.find('input', {'name': 'execution'})['value']
-        _eventId = soup.find('input', {'name': '_eventId'})['value']
-        rmShown = soup.find('input', {'name': 'rmShown'})['value']
-        login_data={
-            'username': input('input account:'),
-            'password': input('input passwd:'),
-            'btn':'',
-            'lt': lt,
-            'dllt': dllt,
-            'execution': execution,
-            '_eventId': _eventId,
-            'rmShown': rmShown
-        }
-        return login_data,response.cookies
-
-    def ucard_login_data_parser(self,url):
-        
-        response=requests.get(url)
-        html=response.text
-        # parse the html
-        soup=BeautifulSoup(html,'lxml')
-        print(soup.prettify())
-        next_url = soup.find('input', {'name': 'NextUrl'})['value']
-        check_code=''
-        img_url= soup.find('img', {'id': 'imgCheckCode'})['src']
-        img_url='http://api.xzxyun.com/Account/'+img_url
-        img=self.get_img(img_url,'check_code')
-
-        print(img)
-        #'http://api.xzxyun.com/Account/Login/Account/GetCheckCodeImg?rad=9'
-        #http://api.xzxyun.com/Account/GetCheckCodeImg?rad=52
-
-        openid=''
-        login_data={
-            'SchoolCode':'xahu',#input('input your school name'),
-            'SignType':'SynSno',#input('input your SignType,"SynSno" or "SynCard"')
-            'UserAccount':input('input your User Account'),
-            'Password':input('input your Password'),
-            'NextUrl':next_url,
-            'CheckCode':check_code,
-            'openid':openid,
-        }
-        return login_data,response.cookies
+    
 
 
 
@@ -136,13 +158,14 @@ if __name__ == "__main__":
     login_url="http://ids.chd.edu.cn/authserver/login?service=http%3A%2F%2Fportal.chd.edu.cn%2F"
     target_url="http://portal.chd.edu.cn/"
 
+    bulletin_url='http://portal.chd.edu.cn/detach.portal?.pmn=view&.ia=false&action=bulletinsMoreView&search=true&.f=f40571&.pen=pe65&groupid=all'
+
     login_url_ucard='http://api.xzxyun.com/Account/Login/'
 
     img_url='https://HaneChiri.github.io/blog_images/article/simple_inverted_index.png'
     sp=spider()
-    #sp.login(login_url,chd_login_data_parser,target_url)
-    #sp.login(login_url_ucard,sp.ucard_login_data_parser)
-    #sp.get_img(img_url)
+    sp.login(login_url,sp.parser.chd_login_data_parser,target_url)
+    sp.parser.chd_get_urls(bulletin_url,cookies=sp.cookies,headers=sp.headers)
 
 
 
